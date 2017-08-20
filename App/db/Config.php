1bea70f29cbb88e92b5656aa795a1b194b13d847
capimobile <nusa1.reload@gmail.com>
2017-08-20 21:03:35 +0700

    rubah file config

--- App/db/Config.php
+++ App/db/Config.php
@@ -3,7 +3,7 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 0);
 
-define("HTTP_HOST", "http://".$_SERVER['HTTP_HOST']."/dev-jakarta/");
+define("HTTP_HOST", "http://".$_SERVER['HTTP_HOST']."/jakarta/");
 
 define("DBPATH","localhost");
 define("DBUSER","agpg");
